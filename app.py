d2f7166eddc7b13feae9fb0997493151f1908488
mekelnih4 <mekelnih3@gmail.com>
2026-03-05 10:42:02 +0700

    Update app.py to include Xoffset and Yoffset variables

--- app.py
+++ app.py
@@ -1,62 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
-import os
+import streamlit as st
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
+# Title
+st.title('Mesin Pengeboran')
 
 # DB Section
-class Data(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    Xoffset = db.Column(db.Float, nullable=False)
-    Yoffset = db.Column(db.Float, nullable=False)
-    DSKI = db.Column(db.Float, nullable=False)
-    AMB = db.Column(db.Float, nullable=False)
-    TOP = db.Column(db.Float, nullable=False)
+L4_Xoffset = 0
+L4_Yoffset = 0
+L5_Xoffset = 0
+L5_Yoffset = 0
+L6_Xoffset = 0
+L6_Yoffset = 0
 
-    def __repr__(self):
-        return f'<Data {self.id}>\n'
+# Example inputs
+L4_value = st.slider('L4 Value', 0, 100)
+L5_value = st.slider('L5 Value', 0, 100)
+L6_value = st.slider('L6 Value', 0, 100)
 
-# DSKI Section
-@app.route('/dski', methods=['GET', 'POST'])
-def dskiengine():
-    if request.method == 'POST':
-        xoffset = request.form.get('xoffset')
-        yoffset = request.form.get('yoffset')
-        dski = request.form.get('dski')
-        new_data = Data(Xoffset=xoffset, Yoffset=yoffset, DSKI=dski)
-        db.session.add(new_data)
-        db.session.commit()
-        flash('Data has been added!', 'success')
-        return redirect(url_for('dskiengine'))
-    return render_template('dski.html')
-
-# AMB Section
-@app.route('/amb', methods=['GET', 'POST'])
-def amb():
-    if request.method == 'POST':
-        amb = request.form.get('amb')
-        new_data = Data(AMB=amb)
-        db.session.add(new_data)
-        db.session.commit()
-        flash('Data has been added!', 'success')
-        return redirect(url_for('amb'))
-    return render_template('amb.html')
-
-# TOP Section
-@app.route('/top', methods=['GET', 'POST'])
-def top():
-    if request.method == 'POST':
-        top = request.form.get('top')
-        new_data = Data(TOP=top)
-        db.session.add(new_data)
-        db.session.commit()
-        flash('Data has been added!', 'success')
-        return redirect(url_for('top'))
-    return render_template('top.html')
-
-if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+# Output with offsets
+st.write(f'L4 Output: {L4_value + L4_Xoffset}, Y Offset: {L4_Yoffset}')
+st.write(f'L5 Output: {L5_value + L5_Xoffset}, Y Offset: {L5_Yoffset}')
+st.write(f'L6 Output: {L6_value + L6_Xoffset}, Y Offset: {L6_Yoffset}')
